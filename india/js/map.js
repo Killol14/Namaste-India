227b4002fd333e16910d48f309e25e646dd8f994
Killol14 <kgselectrical.plumbing@gmail.com>
2023-02-23 01:53:55 +0000

    commiting for google Map API link in index.html

--- india/js/map.js
+++ india/js/map.js
@@ -1,24 +1,30 @@
+// This example adds a marker to indicate the position of Bondi Beach in Sydney,
+// Australia.
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: { lat: 46.619261, lng: -33.134766 },
-    });
-   
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   
-  var locations = [
-    { lat: -31.56391, lng: 147.154312 },
-    { lat: -33.718234, lng: 150.363181 },
-    { lat: -33.727111, lng: 150.371124 },
-    { lat: -33.848588, lng: 151.209834 },
-    
-  ];
-  var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        postition: location,
-        label: labels[i % labels.length]
-    });
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: -33, lng: 151 },
   });
-  const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+const locations = [
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+];
+
+const markers = locations.map(function(location, i){
+  return new google.maps.Marker({
+    position: location,
+    label: labels[i % labels.length]
+  });
+});
+
+var markerCluster = new markerCluster(map, markers,
+  {
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+  });
 }
+  
