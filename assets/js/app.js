3b7f53f7d3759460d8bbcfb4475fe2236ffab203
Killol14 <kgselectrical.plumbing@gmail.com>
2023-03-30 21:54:07 +0000

    this will commit app.js file

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var toggleBtn = document.querySelector(".toggle_btn");
+var toggleBtnIcon = document.querySelector(".toggle_btn i");
+var dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    var isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark bar'
+    : 'fa-solid fa-bars bar'
+}
